5c04cdc7a6873de26f9a7b64727a00e8c368a818
Alexander Nedashkovsky <texnik88@gmail.com>
2017-12-16 13:56:55 +0300

    Fix date to new year calculation

--- src/modules/bot/services/BotService.ts
+++ src/modules/bot/services/BotService.ts
@@ -6,7 +6,7 @@ import {Utils} from '../../common/services/Utils';
 class BotService {
     public static getGreetMessage(): string {
         const todayDate = new Date();
-        const newYearDate = new Date(2018, 1, 1);
+        const newYearDate = new Date(2018, 0, 1);
         const daysToNewYear = Utils.countDaysBetween(todayDate, newYearDate);
 
         return BotService.getGreeterString(daysToNewYear);
@@ -14,9 +14,9 @@ class BotService {
 
     private static getGreeterString(daysToNewYear: number): string {
         return `\
-            Hello!
-            Before the new year is left ${daysToNewYear} days!
-            You can get here new year congratulations.
+            Hello!\n\
+            Before the new year is left ${daysToNewYear} days!\n\
+            You can get here new year congratulations.\n\
             Or you can write your own here: http://path.to.landing/\
         `;
     }
--- src/modules/common/services/Utils.ts
+++ src/modules/common/services/Utils.ts
@@ -5,10 +5,10 @@ class Utils {
     public static countDaysBetween(startDate: Date, endDate: Date): number {
         const DAY_LENGTH_MILISECONDS = 1000 * 60 * 60 * 24;
 
-        const startDateMiliSeconds = startDate.getMilliseconds();
-        const endDateMiliSeconds = endDate.getMilliseconds();
+        const startDateMiliseconds = startDate.getTime();
+        const endDateMiliseconds = endDate.getTime();
 
-        const difference = endDateMiliSeconds - startDateMiliSeconds;
+        const difference = Math.abs(endDateMiliseconds - startDateMiliseconds);
 
         return Math.round(difference/DAY_LENGTH_MILISECONDS);
     }
