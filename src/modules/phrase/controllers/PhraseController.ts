03833d263cd320a3ef519f60c4b683f965bae50b
Alexander Nedashkovsky <texnik88@gmail.com>
2017-12-21 18:53:23 +0300

    Fix format of random generated text answer

--- src/modules/phrase/controllers/PhraseController.ts
+++ src/modules/phrase/controllers/PhraseController.ts
@@ -10,7 +10,7 @@ class PhraseController {
     public async actionGetGeneratedPhrase(request: Express.Request, response: Express.Response): Promise<void> {
         const result = await PhraseService.getRandomPhrase();
         response.contentType('application/json');
-        response.send({result});
+        response.send({...result});
     }
 
     public async actionCreate(request: Express.Request, response: Express.Response): Promise<void> {
